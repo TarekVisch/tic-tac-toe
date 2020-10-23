e6bdb7419845e14aa9d64bbc92a2aedfbb57a5ac
Tarek Chentouf <tarek.chentouf.99@gmail.com>
2020-10-23 19:30:26 +0100

    Move click handling to GameModule

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-const BoardModule = (function () {
+const BoardModule = (function (players) {
   // State
   const board = ['', '', '', '', '', '', '', '', ''];
 
@@ -6,35 +6,10 @@ const BoardModule = (function () {
   const boardDOM = document.querySelector('.board');
   const squares = Array.from(document.querySelectorAll('.board__square'));
 
-  // Events Handling
-  boardDOM.addEventListener('click', handleClick);
-
   function render() {
     for (let i = 0; i < board.length; i++) {
       squares[i].textContent = board[i];
     }
-  }
-
-  function handleClick(e) {
-    const { target } = e;
-
-    if (!target.matches('.board__square')) {
-      return;
-    }
-
-    // If a board square is not empty retutn
-    if (target.textContent != '') {
-      return;
-    }
-
-    // Get index of square
-    const squareIndex = target.dataset.index;
-    // Update board array
-    board[squareIndex] = 'x'; // change this to player mark
-
-    render();
-
-    checkWinner();
   }
 
   function checkWinner() {
@@ -43,14 +18,15 @@ const BoardModule = (function () {
     return winner;
   }
 
-  return { render };
+  return { board, boardDOM, render, checkWinner };
 })();
 
 const playerFactory = (mark, name, score) => {
   // Dom Cache
   const playerDOM = document.querySelector(`.player${mark}`);
-  const details = playerDOM.querySelector('.dashboard__player--details');
+  const detailsDOM = playerDOM.querySelector('.dashboard__player--details');
   const winnerDOM = playerDOM.querySelector('.dashboard__player--winner');
+  const tieDOM = document.querySelector('.dashboard__tie');
 
   // Getters and Setters
   const getMark = () => mark;
@@ -60,36 +36,69 @@ const playerFactory = (mark, name, score) => {
 
   const takeTurn = (isTurn = true) => {
     if (isTurn) {
-      details.classList.add('turn');
+      detailsDOM.classList.add('turn');
     } else {
-      details.classList.remove('turn');
+      detailsDOM.classList.remove('turn');
     }
   };
 
   const winner = (isWinner = true) => {
     if (isWinner) {
-      details.classList.remove('turn');
-      details.classList.add('winner');
+      detailsDOM.classList.remove('turn');
+      detailsDOM.classList.add('winner');
       winnerDOM.style.opacity = 1;
     } else {
-      details.classList.remove('winner');
+      detailsDOM.classList.remove('winner');
       winnerDOM.style.opacity = 0;
     }
   };
 
-  return { getMark, getName, getScore, setScore, takeTurn, winner };
+  return { getMark, getName, getScore, setScore, takeTurn, winner, tieDOM };
 };
 
 const GameModule = (function () {
   const player1 = playerFactory('x', 'Player 1', 0);
   const player2 = playerFactory('o', 'Player 2', 0);
   const players = [player1, player2];
-  let currentPlayer = 0;
+
+  let current = 0;
+  const { board, boardDOM, render, checkWinner } = BoardModule;
+
+  // Events Handling
+  boardDOM.addEventListener('click', handleClick);
 
   function _init() {
-    BoardModule.render();
-    players[currentPlayer].takeTurn();
+    render();
+    player1.takeTurn();
+  }
+  _init();
+
+  function handleClick(e) {
+    const { target } = e;
+
+    if (!target.matches('.board__square')) {
+      return;
+    }
+
+    if (target.textContent != '') {
+      return;
+    }
+
+    // Get index of clicked square
+    const squareIndex = target.dataset.index;
+    // update board
+    board[squareIndex] = players[current].getMark();
+
+    render();
+
+    checkWinner();
+
+    playerNext();
   }
 
-  _init();
+  function playerNext() {
+    players[current].takeTurn(false);
+    current = current === 0 ? 1 : 0;
+    players[current].takeTurn();
+  }
 })();
