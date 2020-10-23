2e854be91fdd7ab09c31c65eba8ece57505a2838
Tarek Chentouf <tarek.chentouf.99@gmail.com>
2020-10-23 12:50:20 +0100

    Add click handler to add marks on the board

--- script/app.js
+++ script/app.js
@@ -1,12 +1,38 @@
 const BoardModule = (function () {
-  const board = ['x', '', 'o', 'o', 'x', 'x', '', '', 'o'];
+  // State
+  const board = ['', '', '', '', '', '', '', '', ''];
 
+  // DOM Cache
+  const boardDOM = document.querySelector('.board');
   const squares = Array.from(document.querySelectorAll('.board__square'));
+
+  // Events Handling
+  boardDOM.addEventListener('click', handleClick);
 
   function render() {
     for (let i = 0; i < board.length; i++) {
       squares[i].textContent = board[i];
     }
+  }
+
+  function handleClick(e) {
+    const { target } = e;
+
+    if (!target.matches('.board__square')) {
+      return;
+    }
+
+    // If a board square is not empty retutn
+    if (target.textContent != '') {
+      return;
+    }
+
+    // Get index of square
+    const squareIndex = target.dataset.index;
+
+    board[squareIndex] = 'x'; // change this to player mark
+
+    render();
   }
 
   return { render };
