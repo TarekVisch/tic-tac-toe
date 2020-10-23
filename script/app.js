2b28bbd870e07b355ad1c44138eaa82cc0b26c5a
Tarek Chentouf <tarek.chentouf.99@gmail.com>
2020-10-23 12:32:27 +0100

    Add a board module with public render method and a game module with initialization of the render

--- script/app.js
+++ script/app.js
@@ -1,1 +1,21 @@
-console.log('Tic Tac Toe');
+const BoardModule = (function () {
+  const board = ['x', '', 'o', 'o', 'x', 'x', '', '', 'o'];
+
+  const squares = Array.from(document.querySelectorAll('.board__square'));
+
+  function render() {
+    for (let i = 0; i < board.length; i++) {
+      squares[i].textContent = board[i];
+    }
+  }
+
+  return { render };
+})();
+
+const GameModule = (function () {
+  function _init() {
+    BoardModule.render();
+  }
+
+  _init();
+})();
